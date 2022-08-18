8ce3dc57591d435937f1afed4fc252015ba8a217
Hjyuni <hjy072911@gmail.com>
2022-08-18 21:47:51 +0900

    node.js basic

---
+++ Vue&Node.js/Node.js/README.md
@@ -0,0 +1,31 @@
+# Node.js
+
+> 생활코딩: https://opentutorials.org/course/3332
+
+## 0. install (nvm으로..)
+
+> nvm으로 node깔기: https://scv-life.tistory.com/191
+>
+> 참고사이트: https://heynode.com/tutorial/install-nodejs-locally-nvm/
+
+* nvm 깔기(**window ver**)
+  * https://github.com/coreybutler/nvm-windows/releases 들어가서 nvm-setup.zip 다운
+  * zip파일 안에 exe 실행해서 nvm깔기
+* 확인(powershell 관리자권한)
+
+```powershell
+# nvm 버전 확인
+nvm version
+# 최신 버전으로 node 깔기
+# nvm은 다양한 노드 버전 깔기 가능(nvm install [version])
+nvm install latest
+nvm use latest
+node --version
+# nvm list로 node 버전 뭐가 깔려있나 확인 가능
+nvm list
+```
+
+---
+
+
+
---
+++ Vue&Node.js/Node.js/main.js
@@ -0,0 +1,27 @@
+let http = require('http');
+let fs = require('fs');
+let url = require('url');
+
+let app = http.createServer(function(request,response){
+    let _url = request.url;
+    let queryData = url.parse(_url, true).query;
+    console.log(queryData);
+    if(_url == '/'){
+      _url = '/index.html';
+    }
+    if(_url == '/favicon.ico'){
+      response.writeHead(404);
+      response.end();
+      return;
+      // return response.writeHead(404);
+    }
+    response.writeHead(200);
+    console.log(__dirname + _url)
+    console.log(_url)
+    // readFileSync: 사용자가 요청할 때마다 경로에 대한 파일을 읽어들여서 그 값을 가져옴
+    // 'readFile' VS 'readFileSync': https://balmostory.tistory.com/33
+    response.end(fs.readFileSync(__dirname + _url));
+ 
+});
+// 기본값 80:webserver
+app.listen(3000);
