b60438a00e084502e42adf418b859a687d7f6a1a
Hjyuni <hjy072911@gmail.com>
2022-08-19 17:10:05 +0900

    node.js dynamic web

--- Vue&Node.js/Node.js/README.md
+++ Vue&Node.js/Node.js/README.md
@@ -27,5 +27,38 @@ nvm list
 
 ---
 
+## 1. module
 
+> exports & require: https://digiconfactory.tistory.com/entry/Nodejs-%EB%AA%A8%EB%93%88%EC%82%AC%EC%9A%A9%EB%B2%95
+>
+> url모듈 사용하기
+>
+> * https://m.blog.naver.com/PostView.naver?isHttpsRedirect=true&blogId=pjok1122&logNo=221522864528
+> * https://opentutorials.org/module/938/7369
+>
+> fs모듈: https://opentutorials.org/module/938/7373
+
+* require: 파이썬의 import와 같음
+* `url`: url 정보를 객체로 가져와 분석하거나(parse) utl 객체를 문자열로 바꿔주는(format, resolve)를 수행
+  * `url.parse([url var], true)`: query속성을 **객체** 형식으로 가져옴
+
+* `fs (FileSystem)` : 파일 관련 모듈
+
+---
+
+## 2. CRUD
+
+* READ :https://nodejs.dev/en/learn/reading-files-with-nodejs
+
+```js
+const fs = require('fs');
+
+fs.readFile('/Users/joe/test.txt', 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
+```
 
--- Vue&Node.js/Node.js/main.js
+++ Vue&Node.js/Node.js/main.js
@@ -1,13 +1,19 @@
+// require: 외부의 기능을 사용하기 위해서 모듈을 가져옴
+// fs(FileSystem): 파일 처리와 관련된 모듈
+// let args = process.argv;console.log(args); : 시작시 실행 인자 받기
 let http = require('http');
 let fs = require('fs');
-// let url = require('url');
+let url = require('url');
 
 let app = http.createServer(function(request,response){
     let _url = request.url;
-    // let queryData = url.parse(_url, true).query;
-    console.log(queryData);
+    let queryData = url.parse(_url, true).query;
+    let title = queryData.id;
+    // console.log(url.parse(_url,true))
+    // console.log(queryData.id);
     if(_url == '/'){
-      _url = '/index.html';
+      // _url = '/index.html';
+      title = 'welcome'
     }
     if(_url == '/favicon.ico'){
       response.writeHead(404);
@@ -15,13 +21,35 @@ let app = http.createServer(function(request,response){
       return;
       // return response.writeHead(404);
     }
+    // status:200(정상 응답)
     response.writeHead(200);
-    console.log(__dirname + _url)
-    console.log(_url)
-    // readFileSync: 사용자가 요청할 때마다 경로에 대한 파일을 읽어들여서 그 값을 가져옴
-    // 'readFile' VS 'readFileSync': https://balmostory.tistory.com/33
-    response.end(fs.readFileSync(__dirname + _url));
- 
+    fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+      const template = `
+      <!doctype html>
+      <html>
+      <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8">
+      </head>
+      <body>
+        <h1><a href="/">WEB</a></h1>
+        <ol>
+          <li><a href="/?id=HTML">HTML</a></li>
+          <li><a href="/?id=CSS">CSS</a></li>
+          <li><a href="/?id=JavaScript">JavaScript</a></li>
+        </ol>
+        <h2>${title}</h2>
+        <p>${description}</p>
+      </body>
+      </html>
+`
+  // console.log(__dirname + _url)
+  // console.log(_url)
+  // readFileSync: 사용자가 요청할 때마다 경로에 대한 파일을 읽어들여서 그 값을 가져옴
+  // 'readFile' VS 'readFileSync': https://balmostory.tistory.com/33
+  // response.end(fs.readFileSync(__dirname + _url));
+  response.end(template);
+    })
 });
 // 기본값 80:webserver
 app.listen(3000);
