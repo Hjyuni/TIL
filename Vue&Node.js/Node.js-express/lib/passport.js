f7962cf8a448c86972c4a653674c25b8004dd2da
Hjyuni <hjy072911@gmail.com>
2022-08-27 20:15:38 +0900

    node.js naver social login

--- Vue&Node.js/Node.js-express/lib/passport.js
+++ Vue&Node.js/Node.js-express/lib/passport.js
@@ -1,7 +1,6 @@
 let db = require('../lib/db');
 // npm install --save bcryptjs && npm uninstall --save bcryptjs
 let bcryptjs = require('bcryptjs');
-let naverInfo = require('../config/naver.json')
 let shortid = require('shortid');
 
 module.exports = (app) => {
@@ -9,7 +8,8 @@ module.exports = (app) => {
   // npm install -S passport-local
   let passport = require('passport')
   let LocalStrategy = require('passport-local').Strategy
-  let  GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+  let GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+  let NaverStrategy = require('passport-naver').Strategy;
 
   app.use(passport.initialize());
   app.use(passport.session());
@@ -51,6 +51,7 @@ module.exports = (app) => {
       }
   ));
 
+  // google social login
   let googleCredentials = require('../config/google.json');
   passport.use(new GoogleStrategy(
     {
@@ -88,6 +89,47 @@ module.exports = (app) => {
 
   app.get('/auth/google/callback',
   passport.authenticate('google', {
+      failureRedirect: '/auth/login'
+  }),
+  (req, res) => {
+      res.redirect('/');
+  });
+
+  // naver social login
+  let naverCredentials = require('../config/naver.json');
+  passport.use(new NaverStrategy(
+    {
+      clientID: naverCredentials.naver.client_id,
+      clientSecret: naverCredentials.naver.client_secret,
+      callbackURL: naverCredentials.naver.redirect_uris
+    },
+    function(accessToken, refreshToken, profile, done) {
+      let email = profile.emails[0].value;
+      let user = db.get('users').find({email:email}).value();
+      // 기존 user가 존재한다면
+        if(user){
+          user.naverId = profile.id;
+          db.get('users').find({id:user.id}).assign(user).write();
+      } else {
+          user = {
+              id:shortid.generate(),
+              email:email,
+              displayName:profile.displayName,
+              naverId:profile.id
+          }
+          db.get('users').push(user).write();
+      }
+      done(null, user);
+    }
+  ));
+
+  app.get('/auth/naver',
+  passport.authenticate('naver', {
+      scope: ['https://nid.naver.com/oauth2.0/authorize','email']
+  }));
+
+  app.get('/auth/naver/callback',
+  passport.authenticate('naver', {
       failureRedirect: '/auth/login'
   }),
   (req, res) => {
