0cd29fd5c61a1717191375d240ec8be6821c79ed
Hjyuni <hjy072911@gmail.com>
2022-10-10 10:58:01 +0900

    django search

--- Django/studydjango/instagram/views.py
+++ Django/studydjango/instagram/views.py
@@ -1,3 +1,16 @@
+# render : templates를 쓰는 인자
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+def post_list(req):
+  qs = Post.objects.all()
+  # q는 검색어 : q가 있으면 q에 담긴 값, 없으면 ''을 가져와줘
+  q = req.GET.get('q','')
+  if q :
+   qs = qs.filter(message__icontains=q)
+  # instagram/templates/instagrm/post_list.html
+  return render(req, 'instagram/post_list.html', {
+    'post_list' : qs,
+    'q': q,
+  }) 
