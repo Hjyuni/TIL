3ceac61c755bb3eed3bcd35c6f555106335ea45d
Hjyuni <hjy072911@gmail.com>
2022-10-13 12:28:45 +0900

    django foreingkey

--- Django/studydjango/instagram/admin.py
+++ Django/studydjango/instagram/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 from django.utils.safestring import mark_safe
 # Register your models here.
 
@@ -35,3 +35,6 @@ class PostAdmin(admin.ModelAdmin):
   # def message_length(self, post):
   #   return len(post.message)
   
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+  pass
--- Django/studydjango/instagram/models.py
+++ Django/studydjango/instagram/models.py
@@ -1,9 +1,9 @@
-from email.policy import default
-from tabnanny import verbose
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
 class Post(models.Model):
+  author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
   message = models.TextField()
   # upload_to에 함수사용 가능
   photo = models.ImageField(blank=True,upload_to='instagram/post/%Y%m%d')
@@ -25,3 +25,12 @@ class Post(models.Model):
   class Meta:
     # orderby: id 역순
     ordering = ['-id']
+
+class Comment(models.Model):
+  # 외래키 지정법: models.ForeignKey(to, on_delete=)
+  # to: 대상모델, 클래스를 직접 지정하거나 클래스 명을 문자열로 지정. 자기 참조는 self
+  # on_delete: record 삭제 시 rule (CASCADE, PROTECT, SET_NULL, SET_DEFAULT, SET, DO_NOTHING)
+  post = models.ForeignKey(Post, on_delete = models.CASCADE) # post_id 필드 생성
+  message = models.TextField()
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateField(auto_now=True)
