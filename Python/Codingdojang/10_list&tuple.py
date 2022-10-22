26f117cfa7717e4288153d46b4a042eb8a428279
Hjyuni <hjy072911@gmail.com>
2022-10-22 10:28:13 +0900

    codingdojang unit 10

---
+++ Python/Codingdojang/10_list&tuple.py
@@ -0,0 +1,21 @@
+# list
+# 빈리스트 생성
+a = []
+b = list()
+# range활용해서 리스트 만들기
+c = list(range(2, 10))
+d = list(range(-4, 12, 2))
+
+# tuple: 안에 저장된 요소를 변경, 추가, 삭제할 수 업음
+# a = (값,값,값) / a = 값, 값, 값
+d = 1,2,3,4,5
+e = (1,2,3,4,5)
+# 요소가 한 개 있는 튜플 만들기
+f = (6, )
+g = 7,
+# range 활용하여 튜플 만들기
+h = tuple(range(10))
+
+print(list('hello'))
+print(tuple('hello'))
+
